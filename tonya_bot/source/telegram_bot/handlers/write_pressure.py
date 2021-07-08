f938743bc0e11529d551883d137998bd40845fe9
IRaccoonI <36865087+IRaccoonI@users.noreply.github.com>
2021-07-08 21:31:13 +0300

    Update write_pressure.py
    feat: Add comments

--- tonya_bot/source/telegram_bot/handlers/write_pressure.py
+++ tonya_bot/source/telegram_bot/handlers/write_pressure.py
@@ -33,9 +33,13 @@ def is_int(s):
     except ValueError:
         return False
 
+# Получение текста из файла расположенного по ссылке
 def get_text_from_voice_url(file_url):
+    # Получение данных голосового сообщения в виде бинарного кода из ссылки
     answer = requests.get(file_url)
+    # Инициализирование клиента для распознования текста из голосового сообщения 
     client = speech.SpeechClient()
+    # Помещение бинарных данных как интересуемый контент
     audio = speech.RecognitionAudio(content=answer.content)
     config = speech.RecognitionConfig(
         encoding=speech.RecognitionConfig.AudioEncoding.OGG_OPUS,
@@ -47,21 +51,30 @@ def get_text_from_voice_url(file_url):
     for result in response.results:
         return result.alternatives[0].transcript
 
+# Наождение цифр в сроке
+def find_numbers(sentence):
+    words = re.findall(r'\d+', sentence)
+    return [int(word) for word in words if word.isdigit()]
+
+# Есть ли в строке 3 числа
+def check_messge_val(text):
+    vals = find_numbers(text)
+    return len(vals) == 3
+
+# Класс состояний
 class WritePressure(StatesGroup):
     waiting_for_val = State()
     waiting_for_accept = State()
     waiting_for_tags = State()
 
+# Начала сценария сбора показателй танометра
 async def write_pressure_start(message: types.Message):
     kb = ReplyKeyboardMarkup(resize_keyboard=True)
     kb.add(strings.write_pressure_cancle_btn)
     await message.answer(strings.write_pressure_input_val, parse_mode='MarkdownV2', reply_markup=kb)
     await WritePressure.next()
 
-def find_numbers(sentence):
-    words = re.findall(r'\d+', sentence)
-    return [int(word) for word in words if word.isdigit()]
-
+# Обработка текста показаний
 async def write_pressure_input_val(message: types.Message, state: FSMContext):
     vals = find_numbers(message.text)
     if (len(vals) == 3):
@@ -71,16 +84,21 @@ async def write_pressure_input_val(message: types.Message, state: FSMContext):
         kb.row(strings.write_pressure_cancle_btn)
 
         await message.answer(strings.write_pressure_input_tags, parse_mode='MarkdownV2', reply_markup=kb)
+        # запись значений танометра в переменную состояния
         await state.update_data(vals=vals)
         await WritePressure.next()
         await WritePressure.next()
     else:
         await message.answer(strings.write_pressure_input_val_invalid)
 
+# Обработка голосового сосбщения с показаниями
 async def voice_message(message: types.Message, state:FSMContext):
+    # Получение ссылки на файл голосового сообщения
     file_url = await message.voice.get_url()
+    # Получение текста из голосового сообщения
     text = get_text_from_voice_url(file_url)
 
+    # Флаг проверки корректности текста голосового сообщения
     f = check_messge_val(text)
 
     ikb = InlineKeyboardMarkup()
@@ -88,16 +106,19 @@ async def voice_message(message: types.Message, state:FSMContext):
     ikb.add(
         InlineKeyboardButton("Потвердить", callback_data='accept'),
         InlineKeyboardButton("Внести заново", callback_data='repiat'))
-
+    
+    # Случаи при корректности текста голосового сообщения
     if f:
         vals = find_numbers(text)
         await message.answer("Вы сказали: {}".format(text), reply_markup=ikb)
+        # запись значений танометра в переменную состояния
         await state.update_data(vals=vals)
         await WritePressure.next()
     else:
         await message.answer("Вы сказали: {}".format(text))
         await message.answer(strings.write_pressure_input_val_invalid)
 
+# Обработчик подтвержедения корректности голосового сообщения
 async def callback_accapet(callback_query: types.CallbackQuery, state:FSMContext):
     chat_id = callback_query.message.chat.id
     msg_id = callback_query.message.message_id
@@ -111,6 +132,7 @@ async def callback_accapet(callback_query: types.CallbackQuery, state:FSMContext
     await callback_query.message.answer(strings.write_pressure_input_tags, reply_markup=kb)
     await WritePressure.next()
 
+# Обработчик повтора ввода данных танометра
 async def callback_repiat(callback_query: types.CallbackQuery, state:FSMContext):
     chat_id = callback_query.message.chat.id
     msg_id = callback_query.message.message_id
@@ -119,12 +141,9 @@ async def callback_repiat(callback_query: types.CallbackQuery, state:FSMContext)
     await callback_query.message.answer(strings.write_pressure_input_val_invalid)
     await WritePressure.previous()
 
-def check_messge_val(text):
-    vals = find_numbers(text)
-    return len(vals) == 3
-
 url_create_tonometr_report = SERVER_HOST_PROTOCOL + "://" + SERVER_HOST + 'user/set-tonometr-report/'
 
+# Обработчик ввода тега с последующей отправкой данных на сервер и выходом из состояния пользователем 
 async def write_pressure_input_tags(message: types.Message, state: FSMContext):
     await state.update_data(tags=message.text)
     await state.update_data(user_id=message.from_user.id)
