52033c4cab5a1c8b07b22e91e4434110f4c197b5
IRaccoonI <36865087+IRaccoonI@users.noreply.github.com>
2021-07-08 20:41:21 +0300

    Update handlers/general.py
    feat: Add coments to handlers/general.py

--- tonya_bot/source/telegram_bot/handlers/general.py
+++ tonya_bot/source/telegram_bot/handlers/general.py
@@ -1,3 +1,4 @@
+# Импорты
 from os import stat
 from aiogram.types.base import String
 import json
@@ -21,76 +22,98 @@ from source.settings import SERVER_HOST_AUTH_URL, SERVER_HOST_PROTOCOL, SERVER_H
 
 import requests
 
+# заголовок для наших запросов на сервер
 req_headers_json = {
     'Content-type': 'application/json',
     'Accept': 'text/plain',
     'Content-Encoding': 'utf-8'
 }
 
+# Класс состояний
 class SelectTanomenr(StatesGroup):
     waiting_for_input = State()
 
+# Вход в состояние SelectTanomenr, при запуске бота в первый раз
 async def send_welcome(message: types.Message):
-
+    # тело запроса на сервер
     data = {
         "bot_type": "telegram",
         "user_id": str(message.from_user.id)
     }
 
-    print(SERVER_HOST_PROTOCOL + "://" + SERVER_HOST_AUTH_URL)
+    # запрос на сервер, для регистрации пользователя в бд
     response = requests.post(
         SERVER_HOST_PROTOCOL + "://" + SERVER_HOST_AUTH_URL,
         data=json.dumps(data),
         headers=req_headers_json)
 
+    # Вывод сообщений ботом
     await message.answer(strings.start_content, reply_markup=ReplyKeyboardRemove())
     await message.answer(strings.choice_tanometr, parse_mode='MarkdownV2')
     await SelectTanomenr.next()
 
+# Вход в состояние SelectTanomenr
 async def select_tanometr_start(message: types.Message):
+    # Создание клавиатуры для пользователя
     kb = ReplyKeyboardMarkup(resize_keyboard=True)
     kb.add(strings.choice_tanometr_cancle)
+    # Вывод сообщения ботом с созданной ранее клавиатуры
     await message.answer(strings.choice_tanometr, parse_mode='MarkdownV2', reply_markup=kb)
+    # Регистрация нового состояния пользователя
     await SelectTanomenr.next()
 
-
+# Формирование ссылки для отправки даных о танометре пользователя на сервер
 url_set_tonometr = SERVER_HOST_PROTOCOL + "://" + SERVER_HOST + 'user/set-tonometr/'
 
+# Выбор танометра
 async def select_tanometr_input(message: types.Message, state: FSMContext):
-
+    # тело запроса на сервер
     data = {
         "bot_type": "telegram",
         "user_id": str(message.from_user.id),
         "tonometr": message.text
     }
+    # Запрос на сервер, для регистрации текущего танометра пользователя в бд
     response = requests.post(
         url_set_tonometr,
         data=json.dumps(data),
         headers=req_headers_json)
 
-    if response.status_code != 200:
-        print(url_set_tonometr)
-        print(response.status_code)
-        print('****ERROR**** upps in general select_tanometr_input')
-
+    # Вывод сообщений ботом
     await message.answer(strings.choice_tanometr_finish, parse_mode='MarkdownV2', reply_markup=home_kb(message.from_user.id))
+    # Выход пользователя из текущего состояния
     await state.finish()
 
+# Отмена изменения танометра
 async def select_tanometr_cancle(message: types.Message, state: FSMContext):
+    # Вывод сообщений ботом
     await message.answer(strings.choice_tanometr_cancle, parse_mode='MarkdownV2', reply_markup=home_kb(message.from_user.id))
+    # Выход пользователя из текущего состояния
     await state.finish()
 
+# Информирование о окончания данной операции, с измением клавиатуры пользователя 
 async def home_cmd(message: types.Message):
     await message.answer(strings.main_menu, parse_mode='MarkdownV2', reply_markup=home_kb(message.from_user.id))
 
 
+# регистрация всех обрабочиков сообщений
 def register_handlers(dp: Dispatcher):
+    # регистрация обработчика, реагирующий когда пользователь не имеет состояний на команду home, вызывает метод home_cmd
     dp.register_message_handler(home_cmd, commands=['home'], state=default_state)
+    # регистрация обработчика, реагирующий когда пользователь не имеет состояний на команду start, что является первым сообщением боту, вызывает метод send_welcome
     dp.register_message_handler(send_welcome, commands=['start'], state=default_state)
-
+    
+    # Регистрация обработчика, реагирующий когда пользователь не имеет состояний 
+    # на текст хранящийся в strings.choice_tanometr_btn, вызывает метод select_tanometr_start
     dp.register_message_handler(select_tanometr_start, Text(equals=strings.choice_tanometr_btn), state=default_state)
+    # Регистрация обработчика, реагирующий когда пользователь в любом состоянии SelectTanomenr 
+    # на текст хранящийся в strings.choice_tanometr_cancle, вызывает метод select_tanometr_cancle
     dp.register_message_handler(select_tanometr_cancle, Text(equals=strings.choice_tanometr_cancle), state=SelectTanomenr.all_states)
+    # Регистрация обработчика, реагирующий когда пользователь в любом состоянии SelectTanomenr
+    # на команду cancle, вызывает метод select_tanometr_cancle
     dp.register_message_handler(select_tanometr_cancle, commands=['cancle'], state=SelectTanomenr.all_states)
+    # Регистрация обработчика, реагирующий когда пользователь в waiting_for_input состояния SelectTanomenr
+    # на любой текст, вызывает метод select_tanometr_input
     dp.register_message_handler(select_tanometr_input, state=SelectTanomenr.waiting_for_input)
 
 
